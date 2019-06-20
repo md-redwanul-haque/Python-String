9b7773934ae849c9191f8cefd3bfded8ab4abee0
Md.Redwanul haque <32310502+Redwan-anik@users.noreply.github.com>
2019-06-21 01:55:44 +0600

    Add files via upload

---
+++ String Slicing.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 12:17:09 2019
+
+@author: Anik
+"""
+
+#syntax - [start argument : stop argument -1 ]
+lang = "python"
+print ( lang[0:3]) 
+print (lang [0:6])
+
+# Step string 
+print("Redwanul" [0:8:2])
+print("Redwanul" [0::2])
+print("Redwanul" [8::-1])
+
+name= "RedWanul HaqUe"
+length =len(name)                #length method 
+print(length)
+
+print (name.lower())            # print lowar digit 
+print(name.upper())             #fo all upper letter making 
+print(name.title())            #for making title
+
+print(name.count("a"))
+
+
+                       
---
+++ string indexing.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 12:13:43 2019
+
+@author: Anik
+"""
+Language = "python"
+print(Language[5])
+print(Language[-1])
+print(Language[-6])
